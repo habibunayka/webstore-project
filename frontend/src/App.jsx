057b7da35b0d85e8b0a9f257621df10e2216d3f2
Habibunayka Miftah Al-Rizqi <habibunayka@gmail.com>
2024-06-29 11:26:57 +0700

    first-commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,6 @@
+import { BrowserRouter as Router} from "react-dom";
+import { Navbar } from "./components";
+
+<Router>
+    <Navbar />
+</Router>;
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,8 @@
+
+const Navbar = () => {
+  return (
+    <div><h1>pp</h1>navbar</div>
+  )
+}
+
+export default Navbar
